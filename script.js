ff39fea7d4dc19d2b52400a60351c83bd2032fb7
faiza203 <60883181+faiza203@users.noreply.github.com>
2020-07-05 10:32:31 +0500

    insert dropdown div

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
 const text = document.getElementById("input");
 const submit = document.getElementById("submit");
-let search;
+const targets = document.getElementById("targets");
+const dropdown = document.getElementById("dropdown");
+const dropdownMenu = document.getElementById("dropdown-menu");
 const fetchData = async () => {
   const response = await axios.get("http://www.omdbapi.com/", {
     params: {
       apikey: "722a32f0",
-      s : text.value,
+      s: text.value,
     },
   });
-  console.log(response.data);
+  if (response.data.Error){
+     return [];
+  }
+  const movies = response.data.Search;
+  for (let movie = 0; movie < movies.length ; movie++) {
+    const div = document.createElement("div");
+    div.setAttribute("id" , "newDiv")
+    div.innerHTML = `<img src="${movies[movie].Poster}"/>
+    <h1>${movies[movie].Title}</h1>`;
+    text.value = text.value ;
+    if(movies[movie].Poster === "N/A"){
+      div.innerHTML = `<img src="${movies[0].Poster}"/>
+      <h1>${movies[movie].Title}</h1>`;
+    }
+    dropdownMenu.appendChild(div); 
+    dropdownMenu.style.border = " 1px solid #aba7a7"
+    div.addEventListener("click",function(){
+      targets.appendChild(div);
+      text.value = movies[movie].Title    
+      dropdownMenu.remove(div)
+    })
+  }
+
 };
-submit.addEventListener("click" , () =>{
-    fetchData()
-})
+submit.addEventListener("click", () => {
+  fetchData();
+});
